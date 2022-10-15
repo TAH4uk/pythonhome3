211eaac81381f5b46a9955b10ff823d5c0a2e8d6
Sergey Yakovlev <pcs_zero@inbox.ru>
2022-10-15 19:32:59 +0300

    task1

---
+++ task1.py
@@ -0,0 +1,15 @@
+# Задача 1. Создайте файл. Запишите в него N первых элементов последовательности Фибоначчи.
+
+fib = int(input("Введите число первых N элементов последовательности Фибоначчи: "))
+
+def fibonacci(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+data = str(list(fibonacci(fib)))
+
+file = open("fib.txt", "w")
+file.writelines(data)
+file.close
